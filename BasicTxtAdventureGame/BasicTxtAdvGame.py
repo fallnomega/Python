9ebaacd4da94e394f40fee378aa4861bb90b4ae0
fallnomega <fallnomega@gmail.com>
2014-08-27 11:40:08 -0700

    Update to Basic Text Adventure
    Added if conditions that determine if your allowed to move in a
    direction depending on the room your in.

--- BasicTxtAdventureGame/BasicTxtAdvGame.py
+++ BasicTxtAdventureGame/BasicTxtAdvGame.py
@@ -1,15 +1,74 @@
 def menu():
-    print("What direction do you want to go?")
+    print("*********************\nQ to quit\nL to see current Location\nWhat direction do you want to go?")
     print("1: North\n2: West\n3: South\n4: East\n\n")
     choice = input("Which way[N,W,S,E]:")
     if choice.isalpha()==False:
         print("Please select N,W,S, or E")
         choice= menu()
-    choice = choice.upper()  
+    choice = choice.upper()
+    print('*********************\n')
     return choice
 
-def moveTo(location):
-    print ("You are in the " + location[0] + ": " + location[1])
+def moveTo(house,location,direction):
+    if direction=='N':
+        if location[0]=='Entrance/Parlor':
+            location = house[1]
+        elif location[0]=='Library':
+            location = house[0]
+        elif location[0]=='Living Room':
+            location=house[2]
+        elif location[0]=='Bathroom':
+            print('The windows are locked, there is no escape!!!\n\n')
+        elif location[0]=='Kitchen':
+            print('The windows are locked, there is no escape!!!\n\n')
+        elif location[0]=='Dining Room':
+            print('The windows are locked, there is no escape!!!\n\n')
+        return location
+            
+    elif direction =='E':
+        if location[0]=='Entrance/Parlor':
+            location = house[5]
+        elif location[0]=='Library':
+            location = house[4]
+        elif location[0]=='Living Room':
+            print('The windows are locked, there is no escape!!!\n\n')
+        elif location[0]=='Bathroom':
+            location = house[1]
+        elif location[0]=='Kitchen':
+            location = house[2]
+        elif location[0]=='Dining Room':
+            print('The windows are locked, there is no escape!!!\n\n')
+        return location
+            
+    elif direction=='S':
+        if location[0]=='Entrance/Parlor':
+            print('The frontdoor is locked.Once you enter this house, there is no escape!!!\n\n')
+        elif location[0]=='Library':
+            print('The windows are locked, there is no escape!!!\n\n')
+        elif location[0]=='Living Room':
+            print('The windows are locked, there is no escape!!!\n\n')
+        elif location[0]=='Bathroom':
+            location = house[3]
+        elif location[0]=='Kitchen':
+            location = house[4]
+        elif location[0]=='Dining Room':
+            location=house[5]
+        return location
+        
+    else:
+        if location[0]=='Entrance/Parlor':
+            location = house[3]
+        elif location[0]=='Library':
+            print('The windows are locked, there is no escape!!!\n\n')
+        elif location[0]=='Living Room':
+            location = house[4]
+        elif location[0]=='Bathroom':
+            print('The windows are locked, there is no escape!!!\n\n')
+        elif location[0]=='Kitchen':
+            location = house[0]
+        elif location[0]=='Dining Room':
+            location = house[1]
+        return location
 
 house = [["Bathroom",'In the bathroom, someone has dropped a bomb in this room. You may need to call Hazmat'],
          ['Kitchen','This kitchen was designed with functionality in mind, it has an island in the center of it along with standard kitchen appliances'],
@@ -18,13 +77,18 @@ house = [["Bathroom",'In the bathroom, someone has dropped a bomb in this room. 
          ["Entrance/Parlor","Filled with cherry black furniture and reeks of varnish and 409"],
          ['Living Room','The living room has a recliner and a 50 inch HDTV in it.']]
 
-
-
 location = house[4]
-print (location)
-direction = menu()
-print (direction)
-moveTo(location)
+print ("You are currently in the " + location[0] + ": " + location[1]+ '\n\n')
+while True:
+
+    
+    direction = menu()
+    if direction=='Q':
+        break
+    elif direction=='L':
+        print ("\n\n\nYou are currently in the " + location[0] + ": " + location[1]+ '\n\n')        
+    location = moveTo(house,location,direction)
+    
 
 '''
 ______________________________
