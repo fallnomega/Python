441aaa682bf97ff008c96bf43c329815afaca385
fallnomega <fallnomega@gmail.com>
2014-08-27 15:03:50 -0700

    methods defined
    defined a few methods.

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,18 +1,46 @@
 from random import randint
-words = ["Tron","Hello","Goodbye","Lucky","Internet",
-         "Python","Recursion","Class","Variable","Operator"]
+words = ["TRON","HELLO","GOODBYE","LUCKY","INTERNET",
+         "PYTHON","RECURSION","CLASS","VARIABLE","OPERATOR"]
 index = randint(0,9)
-hiddenWord = words[index]
-print(hiddenWord)
+theWord = words[index]
+theWord.upper()
+print(theWord)
 
-def singleChar(letter):
-    pass
+
+
+
+
+def singleChar():
+    letter = input("\n\nGuess a letter: ")
+    letter = letter.upper()
+    return letter
 
 def compare(letter,word):
-    pass
+    for i in word:
+        if letter == i:
+            return "Correct"
+    return "Wrong"
 
 def printLetters(selections):
     pass
 def counter(i):
     pass
 
+def generate(word):
+    result = []
+    for i in range(len(word)):
+        result.append("_")
+    return result
+
+def printGuess(result):
+    for i in result[0:]:
+        print (' '.join(i),end=" ")
+    
+    
+
+
+hiddenWord = generate(theWord)
+printGuess(hiddenWord)
+
+guess = singleChar()
+print ("That is " + compare(guess, theWord))
